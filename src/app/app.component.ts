c6fb4d7cbbde84375dda45619c116d9c65a765c0
andrew lupton <aflupton@gmail.com>
2018-06-05 15:21:52 -0700

    add data-binding methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,26 +23,35 @@ export class AppComponent {
     new Soup('Egyptian "Molokhia"', '25 minutes', 4, 3),
     new Soup('"Mercimek Çorbası" (Turkish red lentils)', '50 minutes', 4, 5)
   ];
+  selectedSoup: Soup = this.soups[0];
+  editSoup(clickedSoup) {
+    this.selectedSoup = clickedSoup;
+  };
   meatTitle = 'Meats';
   meats: Meat[] = [
     new Meat('"Spicy Kreuzberg" lamb kebab (Berliner-style)', '1 hour', 4, 4),
     new Meat('Moroccan lamb shanks with pomegranate sauce', '5 hours', 4, 5),
     new Meat('French Boeuf Bourguignon with pasta', '2 hours', 6, 4)
   ];
+  selectedMeat: Meat = this.meats[0];
+  editMeat(clickedMeat) {
+    this.selectedMeat = clickedMeat;
+  };
   saladTitle = 'Salads';
   salads: Salad[] = [
     new Salad('Lebanese Tabouleh with tomatoes', '45 minutes', 4, 5),
     new Salad('French lentil, beet, and egg salad (served warm)', '1 hour', 6, 4)
   ];
-  editRecipe() {
-    alert("You like that?!")
+  selectedSalad: Salad = this.salads[0];
+  editSalad(clickedSalad) {
+    this.selectedSalad = clickedSalad;
   };
-  starColor(currentSalad) {
-    if (currentSalad.stars === 3) {
+  starColor(currentRecipe) {
+    if (currentRecipe.stars === 3) {
       return "bg-info";
-    } else if (currentSalad.stars === 4) {
+    } else if (currentRecipe.stars === 4) {
       return "bg-warning";
-    } else if (currentSalad.stars === 5) {
+    } else if (currentRecipe.stars === 5) {
       return "bg-danger";
     }
   }
